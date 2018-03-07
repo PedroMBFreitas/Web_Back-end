86fcc4861457a3948e7f14b146b4aa9c5c87b1a7
PedroMBFreitas <pedrombfreitas@gmail.com>
2018-03-07 21:53:39 +0000

    aula2

--- Aula2/Aula2-exercicio1.js
+++ Aula2/Aula2-exercicio1.js
@@ -1,5 +1,5 @@
 function Imc(peso, altura){
-    var imc = peso/ (altura^2);
+    var imc = peso/ (altura*altura);
      if (imc < 18.5){
          console.log("Abaixo do peso normal, com imc de: " + imc);
      }
@@ -15,9 +15,42 @@ function Imc(peso, altura){
 
 }
 
-function ordemInversa(frase){
+function fraseInversa(fraseInv){
+    var frase="";
+    var i = fraseInv.length-1;
+    while(i>=0){
+        frase+=fraseInv[i];
+        i--;        
+    }
+    return frase;
+}
+
+function numDeVogais(vogal){
+    var vogais=["a","e","i","o","u"];
+    var i;
+    var j;
+    var n=0;
+    for (i=0, vogal.length; i++){
+        for (j=0, vogais.length;j++)
+            if(vogal[i]==vogais[j]){
+                n++;
+            }
+        if (n>1){
+            return "palavra tem " + n + "vogais"
+
+        };
+        
+    }
+}
+
+function rectangulo(rect){
+
     
+
+    }
 }
 
 
 Imc(60,1.73);
+var inversa=fraseInversa("ola besta");
+console.log(inversa);
